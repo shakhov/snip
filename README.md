03f21008a8a861d3a44293fa7c2434a43ec271e1
Artem Shakhov <shahov.a.a@gmail.com>
2013-03-31 19:51:00 +0400

    Modify readme and project dependencies

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # shakhov.snip
 
-A Clojure library designed to ... well, that part is up to you.
+SNIP is a set of tools for structural design and analysis according to russian design codes, standards and guidelines.
 
 ## Usage
 
@@ -8,6 +8,6 @@ FIXME
 
 ## License
 
-Copyright © 2013 FIXME
+Copyright © 2013 Artem Shakhov
 
 Distributed under the Eclipse Public License, the same as Clojure.
--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
-(defproject shakhov.snip "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject shakhov.snip "0.0.1-SNAPSHOT"
+  :description "Civil engineering according to russian design codes"
+  :url "http://github.com/shakhov/snip/"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [shakhov.flow "0.1.2"]])
