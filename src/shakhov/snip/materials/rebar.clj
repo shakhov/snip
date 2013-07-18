9affe2b47574e961bf0e3e6951bfadec4b6f25bc
Artem Shakhov <shahov.a.a@gmail.com>
2013-07-18 14:02:09 +0400

    Add materials

---
+++ src/shakhov/snip/materials/rebar.clj
@@ -0,0 +1,6 @@
+(ns shakhov.snip.materials.rebar
+  (:require [shakhov.snip.units :as si]))
+
+(def AIII
+  {:Er (si/MPa 1.96e5)
+   :Rr (si/MPa 350.0)})
---
+++ src/shakhov/snip/materials/steel.clj
@@ -0,0 +1,18 @@
+(ns shakhov.snip.materials.steel
+  (:require [shakhov.snip.units :as si]))
+
+(def st-15HSND
+  {:Est (si/MPa 2.06e5)
+   :Ry  (si/MPa 285.0)
+   :Ru  (si/MPa 400.0)
+   :Ryn (si/MPa 330.0)
+   :Run (si/MPa 470.0)
+   :gamma-m 1.165})
+
+(def st-10HSND
+  {:Est (si/MPa 2.06e5)
+   :Ry  (si/MPa 350.0)
+   :Ru  (si/MPa 450.0)
+   :Ryn (si/MPa 390.0)
+   :Run (si/MPa 510.0)
+   :gamma-m 1.125})
