5d3241a3485623c75715a25057594350cda4ecba
Artem Shakhov <shahov.a.a@gmail.com>
2013-09-09 10:15:05 +0400

    Add missing algo.generic dependency

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :url "http://github.com/shakhov/snip/"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[org.clojure/algo.generic "0.1.1"]
+                 [org.clojure/clojure "1.5.1"]
                  [shakhov.flow "0.1.3-SNAPSHOT"]
                  [shakhov.units "0.1.0-SNAPSHOT"]])
